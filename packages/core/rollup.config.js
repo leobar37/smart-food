d359e06247902686ecb93d5658e212651252e30e
leobar37 <leonardito3459@gmail.com>
2022-06-11 18:05:31 -0500

    fix: deployment

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json';
 const packageJson = require('./package.json');
 
 export default [
@@ -27,6 +28,7 @@ export default [
       resolve({
         preferBuiltins: true,
       }),
+      json(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
     ],
