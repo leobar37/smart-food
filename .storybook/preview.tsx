2cf43c9d7b0b9f1f928b1c458a8fa867ab96e4c6
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-06-24 23:27:00 -0500

    feat: add storybook and demo

---
+++ .storybook/preview.tsx
@@ -0,0 +1,14 @@
+import { Parameters, Story } from '@storybook/react';
+import { theme } from '@smartfood/ui';
+export const parameters: Parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  chakra: {
+    theme: theme,
+  },
+};
