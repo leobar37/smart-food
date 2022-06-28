ce860dc9d871687883e679d8c7dae0095f6bf058
Yeferson Mejias <yefer.el.teke@gmail.com>
2022-06-27 22:43:47 -0500

    feature: add input theme (#6)

--- packages/ui/src/theme/components/index.ts
+++ packages/ui/src/theme/components/index.ts
@@ -1,9 +1,11 @@
 import Button from './button';
 import Radio from './radio';
 import Popover from './popover';
+import Input from './input';
 
 export default {
   Button,
   Radio,
   Popover,
+  Input,
 };
