a424e15f019bc567a6cb4f50a93f500e5af0a439
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-06-27 23:30:01 -0500

    feat: add exotic card

--- packages/ui/src/stories/exotic-card.stories.tsx
+++ packages/ui/src/stories/exotic-card.stories.tsx
@@ -1,9 +1,22 @@
 import { ComponentMeta } from '@storybook/react';
 import { ExoticCard } from '../components/card';
+import { HStack, Link } from '@chakra-ui/react';
 export default {
   title: 'Components / ExoticCard',
 } as ComponentMeta<any>;
 
+const defaultProps = {
+  title: 'Arma tu Poke Bowl',
+  subTitle: 'A sólo s/24.90',
+  link: <Link>Haz Click</Link>,
+  src: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+};
+
 export const Normal = () => {
-  return <ExoticCard />;
+  return (
+    <HStack spacing={4}>
+      <ExoticCard {...defaultProps} variant="large" />
+      <ExoticCard {...defaultProps} variant="small" />
+    </HStack>
+  );
 };
--- packages/ui/src/theme/components/index.ts
+++ packages/ui/src/theme/components/index.ts
@@ -1,9 +1,10 @@
 import Button from './button';
 import Radio from './radio';
 import Popover from './popover';
-
+import ExoticCard from './exotic-card';
 export default {
   Button,
   Radio,
   Popover,
+  ExoticCard,
 };
