e85dbbe4488e3e374da73aff7abe338bc821884a
leobar <60113294+leobar37@users.noreply.github.com>
2022-06-27 23:59:23 -0500

    feat: add exotic card (#7)
    Co-authored-by: LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>

--- packages/ui/src/components/card/ExoticCard.tsx
+++ packages/ui/src/components/card/ExoticCard.tsx
@@ -1,62 +1,74 @@
-import { Box, HStack, Image, Text, VStack, Link } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  Link,
+  useMultiStyleConfig,
+  chakra,
+} from '@chakra-ui/react';
+import { cx } from '@chakra-ui/utils';
+import { FC, ReactNode } from 'react';
+import { motion, Variants, isValidMotionProp } from 'framer-motion';
+type ExoticCardProps = {
+  variant?: 'small' | 'large';
+  src: string;
+  alt?: string;
+  title: string;
+  subTitle: string;
+  link: ReactNode;
+};
 
-export const ExoticCard = () => {
+const cardVariants: Variants = {
+
+};
+
+const imageVariants: Variants = {
+  normal: {
+    scale: 1
+  },
+  hover: {
+    scale: 1.4,
+  },
+};
+const BoxMotion = chakra(motion.div, {
+  shouldForwardProp: prop => isValidMotionProp(prop)  || prop === "children",
+});
+const ImageMotion = chakra(motion.img);
+
+export const ExoticCard: FC<ExoticCardProps> = ({
+  src,
+  alt,
+  title,
+  subTitle,
+  link,
+  ...props
+}) => {
+  const styles = useMultiStyleConfig('ExoticCard', props);
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        width: '326px',
-        height: '326px',
-        zIndex: 4,
-        rounded : "lg",
-        overflow: "hidden"
-      }}
+    <BoxMotion
+      whileHover="hover"
+      __css={styles.card}
+      className={cx('exotic_card')}
     >
-      <Image
-        sx={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          zIndex: 5,
-        }}
-        src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      <ImageMotion
+        __css={styles.image}
+        src={src}
+        variants={imageVariants}
+        className="exotic_card__image"
       />
-
-      <Box
-        sx={{
-          bg: 'transparent',
-          position: 'absolute',
-          zIndex: 6,
-          width: '100%',
-          height: '80px',
-          bottom: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <HStack
-          sx={{
-            bg: 'smartgreen.500',
-            width: '90%',
-            height: '57px',
-            rounded: 'lg',
-            justifyContent :"space-between",
-            px: "3",
-            py : "2",
-            color :"white"
-          }}
-        >
-          <VStack fontWeight={"light"} alignItems={"start"} spacing="0">
-            <Text>Arma tu Poke Bowl</Text>
-            <Text>A sólo s/24.90</Text>
+      <Box __css={styles.box}>
+        <HStack __css={styles.info} className="exotic_card__info">
+          <VStack fontWeight={'light'} alignItems={'start'} spacing="0">
+            <Text>{title}</Text>
+            <Text>{subTitle}</Text>
           </VStack>
-          <VStack alignItems={"start"}  h="full">
-            <Link>Haz clic</Link>
+          <VStack alignItems={'start'} h="full">
+            {link}
           </VStack>
         </HStack>
       </Box>
-    </Box>
+    </BoxMotion>
   );
 };
--- packages/ui/src/theme/styles.ts
+++ packages/ui/src/theme/styles.ts
@@ -4,8 +4,9 @@ import { Styles } from '@chakra-ui/theme-tools';
  * @see https://github.com/chakra-ui/chakra-ui/blob/main/packages/theme/src/styles.ts
  */
 const styles: Styles = {
-
-
+    global : {
+        fontFamily : "body" 
+    }
 };
 
 export default styles;
