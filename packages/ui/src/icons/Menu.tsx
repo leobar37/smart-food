b78874da3fa4f1ef52abb8f05a1a9993542238ee
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-06-29 16:56:36 -0500

    feat: folder structure

---
+++ packages/ui/src/icons/Menu.tsx
@@ -0,0 +1,14 @@
+import { createIcon } from '@chakra-ui/react';
+
+export const MenuIcon = createIcon({
+  viewBox: '0 0 19 15',
+  path: [
+    <path
+      d="M1.393 1.5h16m-16 6h16m-16 6h16"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />,
+  ],
+});
--- packages/ui/src/icons/index.ts
+++ packages/ui/src/icons/index.ts
@@ -1,1 +1,3 @@
 export * from './CheckNotificationIcon';
+export * from './Cart';
+export * from './Menu';
