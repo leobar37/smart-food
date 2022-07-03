9424559f7b4ca92699ce1393e9219babeccb4a67
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-07-03 18:08:47 -0500

    feat: add header and fix isolate modules pattern

--- packages/ui/src/theme/foundations/index.ts
+++ packages/ui/src/theme/foundations/index.ts
@@ -1,10 +1,12 @@
 import breakpoints from './breakpoints';
 import colors from './colors';
-import typography  from './typography'
+import typography from './typography';
+import sizes from './sizes';
 export const foundations = {
   breakpoints,
   colors,
-  ...typography
+  ...typography,
+  sizes,
 };
 
 export default foundations;
