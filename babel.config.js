286b8b2823f9d1179a64e788f9cc21f110aa5106
Yeferson Mejias <yefer.el.teke@gmail.com>
2022-07-12 16:11:42 -0500

    feat: use preconstruct in ui package (#30)

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,11 @@ const BABEL_ENV = process.env.BABEL_ENV;
 const cjs = BABEL_ENV !== undefined && BABEL_ENV === 'cjs';
 
 module.exports = {
-  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+  ],
   presets: [
     '@babel/preset-typescript',
     '@babel/preset-react',
