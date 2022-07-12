0cd5e1cf487b6788357d04c54a3cd72feff28143
Yeferson Mejias <yefer.el.teke@gmail.com>
2022-07-11 22:50:39 -0500

    chore: px to rem, and add yarn.lock to gitignore

--- apps/website/src/modules/landing/components/checkout/SendModal.tsx
+++ apps/website/src/modules/landing/components/checkout/SendModal.tsx
@@ -18,28 +18,28 @@ export const SendModal = () => {
   return (
     <Modal isOpen={true} isCentered onClose={noop}>
       <ModalOverlay />
-      <ModalContent
-        padding={['19px', null, '40px', '48px']}
-        margin="17px"
-        maxWidth="3xl"
-      >
-        <ModalHeader padding="0" mb="32px">
+      <ModalContent padding={['5', null, '10', '12']} margin="4" maxWidth="3xl">
+        <ModalHeader padding="0" mb="8">
           <VStack>
             <Stack
-              width="88px"
-              height="88px"
+              width="5.5rem"
+              height="5.5rem"
               bg="smartgreen.100"
               borderRadius="xl"
               justifyContent="center"
               alignItems="center"
             >
-              <CheckIcon color="smartgreen.500" width="44px" height="44px" />
+              <CheckIcon
+                color="smartgreen.500"
+                width="2.75rem"
+                height="2.17rem"
+              />
             </Stack>
             <Text
               as="h3"
               color="smartgreen.500"
               textAlign={'center'}
-              fontSize={['17px', null, '2xl']}
+              fontSize={['md', null, '2xl']}
             >
               Tu pedido se envió correctamente
             </Text>
@@ -49,17 +49,17 @@ export const SendModal = () => {
           <Text
             as="h5"
             color="smartgray.500"
-            fontSize={['16px', '18px']}
+            fontSize={['md', 'lg']}
             textAlign="center"
             width="100%"
-            mb="32px"
+            mb="8"
           >
             Gracias por preferirnos, el detalle de tu pedido fue enviado a tu
             whatsApp. Por el mismo medio se te enviará el código para que puedas
             realizar el deposito. ¡Disfruta tu pedido!
           </Text>
           <VStack>
-            <Button width="256px" height="48px" colorScheme="smartgray">
+            <Button width="2xs" height="3rem" colorScheme="smartgray">
               Ir a inicio
             </Button>
           </VStack>
