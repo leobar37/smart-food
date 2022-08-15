cc84891b0439b3b104a31632b4c7811632d58fd6
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-08-14 22:55:47 -0500

    fix: change admin url

--- apps/website/src/core/lib/cms.ts
+++ apps/website/src/core/lib/cms.ts
@@ -4,7 +4,7 @@ import { isDev } from '@smartfood/common';
 const client = new ClientV2({
   endpoint: isDev
     ? 'http://localhost:5000/api/graphql'
-    : 'https://admin.smarfood.com/api/graphql',
+    : 'https://app.smarfood.com/api/graphql',
 });
 
 export default client;
