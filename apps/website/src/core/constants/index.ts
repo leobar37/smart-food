23a5d59652be05c8dccd6a9123ebeddc868feda6
LeonBarbozaElmerJoselito <leonbarbozaelmerjoselito@gmail.com>
2022-10-21 21:51:12 -0500

    fix: add new version

--- apps/website/src/core/constants/index.ts
+++ apps/website/src/core/constants/index.ts
@@ -9,7 +9,7 @@ export const mutationsKeys = {
   addLineToOrder: ['addtoCart', 'cart'],
 };
 
-export const PRODUCTS_FOR_BUILD_ID = 'cl7c31wlk0002fguqrk3wfgva';
+export const PRODUCTS_FOR_BUILD_ID = 'cl8tps4wu0002qguqvkkmxwkz';
 
 export const DEMO_IMAGE =
   'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80';
